1ad4790d76f8400df90d1432665506872b54e4ab
Lee Geertsen <lee.geertsen@hotmail.com>
2018-01-24 16:31:58 +0100

    Add socket io connection

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
 
+const io = require('socket.io-client');
 
 export default class App extends React.Component {
   state = {
     fontLoaded: false,
+    isConnected: false,
+    data: null,
   };
 
   async componentWillMount() {
@@ -16,6 +19,24 @@ export default class App extends React.Component {
     });
 
     this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    // INSTRUCTION:
+    // or replace with your local ngrok url, eg: https://brent123.ngrok.io
+    // start ngrok with ngrok http 3000 --subdomain=brent123
+    // where the subdomain is whatever subdomain you want
+    const socket = io('https://slamserver-rkprnuoghc.now.sh/', {
+      transports: ['websocket'],
+    });
+
+    socket.on('connect', () => {
+      this.setState({ isConnected: true });
+    });
+
+    socket.on('ping', data => {
+      this.setState(data);
+    });
   }
 
   render() {
@@ -39,9 +60,11 @@ export default class App extends React.Component {
           </Header>
           <Content>
             <Image source={pic} style={{width: 70, height: 70}}/>
-            <Text>
-              This is Content Section
-            </Text>
+            <Text>connected: {this.state.isConnected ? 'true' : 'false'}</Text>
+            {this.state.data &&
+              <Text>
+                ping response: {this.state.data}
+              </Text>}
           </Content>
           <Footer>
             <FooterTab>
